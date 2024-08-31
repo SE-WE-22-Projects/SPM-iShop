84e7024573a4c70e422906987e7ff906aec09bcd
Navindu Nimsara <navidunimsara@gmail.com>
2024-08-31 12:13:45 +0530

    router initialized

---
+++ server/routes/router.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const register = (systemName, path) => {
+    let routes;
+    try {
+        routes = require(path);
+    } catch (ex) {
+        console.error(`RouteError: Failed to load ${path}`);
+        console.error(ex);
+        return;
+    }
+
+    // register routes from the file
+    try {
+        router.use(`/${systemName}`, routes)
+    } catch (ex) {
+        console.error(`RouteError: An exception was thrown while registering routes in ${path}`)
+        console.error(ex)
+        return;
+    }
+}
+
+register('testService','./testServiceRoute');
+
+module.exports = router;
