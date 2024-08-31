49351f56b328aabfdb14e1849cdb5f65e347b0b3
Navindu Nimsara <navidunimsara@gmail.com>
2024-08-31 12:15:09 +0530

    test service function definitions

---
+++ server/services/testService/testService.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const generateCustomId = require("../../util/sequenceGenerator");
+
+// use this end point for testing purposes
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const testService = async (req,res)=>{
+    
+}
+
+module.exports = {
+    testService
+};
