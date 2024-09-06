066aff93b63f8d4151476035c201de770d64deab
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-07 04:57:05 +0530

    attribute changes

--- web-ui/src/systems/inventory/itemComponents/ItemTable.tsx
+++ web-ui/src/systems/inventory/itemComponents/ItemTable.tsx
@@ -6,6 +6,8 @@ export interface data{
     id?: number;
     name?: string;
     desc?: string;
+    category?: string,
+    unit?: string,
     price?: number;
     qty?: number;
     rackId?: number;
@@ -33,7 +35,9 @@ const ItemTable = ({data, query}:{data: data[], query: string}) => {
                     <TableCell>Item ID</TableCell>
                     <TableCell>Name</TableCell>
                     <TableCell>Description</TableCell>
+                    <TableCell>Category</TableCell>
                     <TableCell>Price</TableCell>
+                    <TableCell>Unit</TableCell>
                     <TableCell>Quantity</TableCell>
                     <TableCell>Rack Id</TableCell>
                     <TableCell size="medium">Action</TableCell>
@@ -56,7 +60,9 @@ const ItemTable = ({data, query}:{data: data[], query: string}) => {
                         <TableCell key={row.id}>{row.id}</TableCell>
                         <TableCell key={row.name}>{row.name}</TableCell>
                         <TableCell key={row.desc}>{row.desc}</TableCell>
+                        <TableCell key={row.category}>{row.category}</TableCell>
                         <TableCell key={row.price}>{row.price}</TableCell>
+                        <TableCell key={row.unit}>{row.unit}</TableCell>
                         <TableCell key={row.qty}>{row.qty}</TableCell>
                         <TableCell key={row.rackId}>{row.rackId}</TableCell>
                         <TableCell size="medium">
