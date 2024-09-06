36bacd0ca71696cc5b59db4fada867251310bd69
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-06 15:20:47 +0530

    inventory main crud implemented

---
+++ server/services/inventoryService/inventoryService.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const Item = require("../../models/Item");
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const createItem = async (req, res) => {
+    const reqData = req.body;
+    try{
+        var item = await Item.create(reqData);
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+
+    res.status(200).json({data: item});
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const getAllItems = async (req,res) => {
+    try{
+        var itemList = await Item.findAll();
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+
+    res.status(200).json(itemList);
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const getItemById = async (req,res) => {
+    const itemId = Number.parseInt(req.params.itemId);
+    // check if Item id is valid
+    if (!Number.isInteger(itemId)) {
+        res.status(400).json({ msg: "Invalid Item ID (format error)" });
+        return;
+    }
+
+    try{
+        var item = await Item.findOne({where: { id: itemId }});
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+
+    res.status(200).json({data: item});
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const updateItemById = async (req,res) => {
+    const itemId = Number.parseInt(req.params.itemId);
+    // check if Item id is valid
+    if (!Number.isInteger(itemId)) {
+        res.status(400).json({ msg: "Invalid Item ID (format error)" });
+        return;
+    }
+
+    try{
+        // check existance of item
+        var item = await Item.findByPk(itemId);
+        if (item === null) {
+            res.status(404).json({ msg: "The Item does not exist" });
+            return;
+        }
+        // update item
+        item = await item.update(
+            {   
+                name: req.body.name,
+                desc: req.body.desc,
+                price: req.body.price,
+                qty: req.body.qty
+            }
+        );
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+
+    res.status(200).json(item);
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const deleteItemById = async (req,res)=> {
+    const itemId = Number.parseInt(req.params.itemId);
+    // check if Item id is valid
+    if (!Number.isInteger(itemId)) {
+        res.status(400).json({ msg: "Invalid Item ID (format error)" });
+        return;
+    }
+    try {
+        // check existance of item
+        let item = await Item.findByPk(itemId);
+        if (item === null) {
+            res.status(404).json({ msg: "The Item does not exist" });
+            return;
+        }
+        // delete item
+        await item.destroy();
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+    
+    res.sendStatus(204);
+}
+
+module.exports = {
+    createItem,
+    getAllItems,
+    getItemById,
+    updateItemById,
+    deleteItemById,
+};
