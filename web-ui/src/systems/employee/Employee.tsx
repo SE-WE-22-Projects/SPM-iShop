758be67a9fcc6390fa6a66b1452de6faa3bd4de4
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-07 11:23:29 +0530

    employee main page

--- web-ui/src/systems/employee/Employee.tsx
+++ web-ui/src/systems/employee/Employee.tsx
@@ -1,8 +1,11 @@
 import { Box, Button, Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "../../common/SearchBar";
 import { Add } from "@mui/icons-material";
-import EmployeeTabel from "./employeeComponents/EmployeeTabel";
+import EmployeeTabel, { EmployeeData } from "./employeeComponents/EmployeeTabel";
+import AddEmployeeeModel, { EmpAddData } from "./employeeComponents/AddEmployeeeModel";
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
 
 const Employee = () => {
     // real time search
@@ -15,6 +18,33 @@ const Employee = () => {
     const employeeAddModalOpen = () => setAddOpen(true);
     const employeeAddModalClose = () => setAddOpen(false);
 
+    // add employee 
+    const addEmployee = async (empData: EmpAddData)=>{
+        // api call
+        try{
+            console.log(empData);
+            await axios.post("api/employee/employee",empData);
+            enqueueSnackbar("Employee added successfuly...", {variant:  "success"});
+            employeeAddModalClose();
+            getEmployee();
+        }
+        catch(e){
+            enqueueSnackbar("failed to add employee...", {variant: "error"});
+            console.error(e);
+        }
+    }
+
+    // get Employee
+    const [empList,setEmpList] = useState<EmployeeData[]>([]);
+    const getEmployee = async ()=>{
+        const res = await axios.get("api/employee/employee");
+        setEmpList(res.data);
+    }
+
+    useEffect(()=>{
+        getEmployee();
+    },[]);
+
     return (
         <div>
             <Typography variant="h3" align="center"> Item Management</Typography>
@@ -25,7 +55,8 @@ const Employee = () => {
                     Add Employee
                 </Button>
             </Box>
-            <EmployeeTabel data={[]} query={searchQuery} />
+            <EmployeeTabel data={empList} query={searchQuery} />
+            <AddEmployeeeModel employeeAddModalClose={employeeAddModalClose} open={addOpen} addEmployee={addEmployee} />
         </div>
     )
 }
