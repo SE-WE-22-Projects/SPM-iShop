320c19ddfe55f431477dc67f5481303d2dcb6144
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-29 18:24:33 +0530

    props changes

--- web-ui/src/systems/inventory/allocationComponents/Allocation.tsx
+++ web-ui/src/systems/inventory/allocationComponents/Allocation.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { itemDataTable } from '../itemComponents/ItemTable'
 import UnallocatedItemTable from './UnallocatedItemTable'
 import Box from '@mui/material/Box'
 import SearchBar from '../../../common/SearchBar'
 
-const Allocation = ({data, allocation, handleClickOpen, handleClose, open}: {data: itemDataTable[], allocation: (itemID: number,rackId: number)=>void, handleClickOpen: ()=>void, handleClose: ()=>void, open: boolean}) => {
+const Allocation = ({data, allocation, handleClickOpen, handleClose, open, rackList}: {data: itemDataTable[], allocation: (itemID: number,rackId: number)=>void, handleClickOpen: ()=>void, handleClose: ()=>void, open: boolean, rackList: any}) => {
   // real time search
   const [searchQuery, setSearchQuery] = useState<string>("");
   const search = (str: string)=>{
@@ -17,7 +17,7 @@ const Allocation = ({data, allocation, handleClickOpen, handleClose, open}: {dat
       <Box sx={{ display: "flex" }} my={2} mx={15} >
           <SearchBar onSearch={search} />
       </Box>
-      <UnallocatedItemTable data={data} allocation={allocation} query={searchQuery} handleClickOpen={handleClickOpen} handleClose={handleClose} open={open}/>
+      <UnallocatedItemTable data={data} allocation={allocation} query={searchQuery} handleClickOpen={handleClickOpen} handleClose={handleClose} open={open} rackList={rackList} />
       </div>
   )
 }
