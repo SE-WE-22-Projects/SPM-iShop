37ff05950db1226aee5498f3c743c6c16bc46576
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-29 17:27:01 +0530

    add allocation page created

---
+++ web-ui/src/systems/inventory/allocationComponents/Allocation.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { itemDataTable } from '../itemComponents/ItemTable'
+import UnallocatedItemTable from './UnallocatedItemTable'
+import Box from '@mui/material/Box'
+import SearchBar from '../../../common/SearchBar'
+
+const Allocation = ({data, allocation, handleClickOpen, handleClose, open}: {data: itemDataTable[], allocation: (itemID: number,rackId: number)=>void, handleClickOpen: ()=>void, handleClose: ()=>void, open: boolean}) => {
+  // real time search
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const search = (str: string)=>{
+      setSearchQuery(str);
+  }
+  
+  return (
+    <div>
+      Allocation
+      <Box sx={{ display: "flex" }} my={2} mx={15} >
+          <SearchBar onSearch={search} />
+      </Box>
+      <UnallocatedItemTable data={data} allocation={allocation} query={searchQuery} handleClickOpen={handleClickOpen} handleClose={handleClose} open={open}/>
+      </div>
+  )
+}
+
+export default Allocation
