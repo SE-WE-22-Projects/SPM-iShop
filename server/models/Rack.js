7484e81cb1ce1e86743a29c299fff6b5d5ad21af
yehan2002 <yehanjaya2002@gmail.com>
2024-09-29 17:32:13 +0530

    Update mapping APIs.

--- server/models/Rack.js
+++ server/models/Rack.js
@@ -3,7 +3,6 @@ const { sequelize } = require(".");
 const Section = require("./Section");
 
 const Rack = sequelize.define("rack", {
-    tag_code: DataTypes.INTEGER,
     top_x: DataTypes.FLOAT,
     top_y: DataTypes.FLOAT,
     bottom_x: DataTypes.FLOAT,
