17e666dd357823fd610188309fbe9b22f73993c3
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-29 17:21:43 +0530

    server function for item allocation

--- server/services/inventoryService/inventoryService.js
+++ server/services/inventoryService/inventoryService.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const Item = require("../../models/Item");
 
+const sq = require("sequelize");
+const Rack = require("../../models/Rack");
+const Op = sq.Op;
+
 /**
  * 
  * @param {express.Request} req 
@@ -132,10 +136,96 @@ const deleteItemById = async (req,res)=> {
     }
 }
 
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const getUnallocatedItems = async (req,res)=> {
+    try{
+        var itemList = await Item.findAll({
+            where: {
+                rackId: null
+            }
+        });
+        res.status(200).json(itemList);
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const getAllocatedItems = async (req,res)=> {
+    try{
+        var itemList = await Item.findAll({
+            where: {
+                rackId: {
+                    [Op.not]: null
+                }
+            }
+        });
+        res.status(200).json(itemList);
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res  
+ */
+const allocateRackToItem = async (req,res)=> {
+    const itemId = Number.parseInt(req.params.itemId);
+    const rackId = req.body.rackId;
+    // check if Item id is valid
+    if (!Number.isInteger(rackId)) {
+        res.status(400).json({ msg: "Invalid Rack ID (format error)" });
+        return;
+    }
+    try{
+        // check existance of item
+        var item = await Item.findByPk(itemId);
+        if(!item){
+            res.status(404).json({ msg: "The Item does not exist" });
+            return;
+        }
+        // check existance of rack
+        const rack = await Rack.findByPk(rackId);
+        if(!rack){
+            res.status(404).json({ msg: "The Rack does not exist" });
+            return;
+        }
+        // allocate rack
+        item = await item.update({
+            rackId: rackId,
+        });
+        res.status(200).json(item);
+    }
+    catch(e){
+        console.error("Error in database operation",e);
+        res.status(500).send("Error in database operation");
+        return;
+    }
+}
+
 module.exports = {
     createItem,
     getAllItems,
     getItemById,
     updateItemById,
     deleteItemById,
+    getUnallocatedItems,
+    getAllocatedItems,
+    allocateRackToItem
 };
