f32c75cdd7d8a9e332c958cb429641a7def532dd
Kasun Hettiarachchi <kasunh1411@gmail.com>
2024-10-03 17:38:06 +0530

    Function created to fetch promotion desc by sectionID

--- server/routes/promotions.js
+++ server/routes/promotions.js
@@ -8,11 +8,13 @@ router.get("/testPromotion", promotion.testPromotion);
 router.post("/promotion", promotion.createPromotion);
 //get all promotions
 router.get("/promotion", promotion.getPromotions);
-//get a promotion by ID 
+//get a promotion by ID
 router.get("/promotion/:id", promotion.getPromotionByID);
 //update a promotion by ID
 router.put("/promotion/:id", promotion.updatePromotion);
 //delete a promotion by ID
 router.delete("/promotion/:id", promotion.deletePromotion);
+//get promotions by category
+router.get("/promotion/section/:sectionID", promotion.getPromotionsBySectionID);
 
 module.exports = router;
--- server/services/promotionService/promotionService.js
+++ server/services/promotionService/promotionService.js
@@ -1,132 +1,168 @@
 const express = require("express");
-const Promo = require('../../models/Promo');
+const Promo = require("../../models/Promo");
+const Rack = require("../../models/Rack");
+const Section = require("../../models/Section");
+const Item = require("../../models/Item");
+const { QueryTypes } = require("sequelize");
+const { sequelize } = require("../../models");
 // import { object, string, number, date } from 'yup';
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const testPromotion = async (req, res) => {
+  res.status(200).send("Testing route for promotions");
+};
 
 /**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
  */
-const testPromotion = async (req, res)=>{
-    res.status(200).send("Testing route for promotions");   
-}
+const createPromotion = async (req, res) => {
+  try {
+    const reqData = req.body;
+
+    const newPromo = await Promo.create(reqData);
+    res.status(201).json({ data: newPromo });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to create promotions" });
+  }
+};
 
 /**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
  */
-const createPromotion = async (req, res)=>{
-   try{
-        const reqData = req.body;
-        
-
-        const newPromo = await Promo.create(reqData);
-        res.status(201).json({data : newPromo});
-   } 
-   catch(error){
-        console.error(error);
-        res.status(500).json({error:'Failed to create promotions'});
-   }   
-}
+const getPromotions = async (req, res) => {
+  try {
+    const promos = await Promo.findAll();
+    res.status(200).json(promos);
+  } catch (error) {
+    res.status(500).send("Failed to fetch promotions");
+  }
+};
 
 /**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
  */
-const getPromotions = async (req, res)=>{
-    try{
-        const promos = await Promo.findAll();
-        res.status(200).json(promos);
+const getPromotionByID = async (req, res) => {
+  try {
+    const promoID = req.params.id;
+    const promoByID = await Promo.findByPk(promoID);
+    if (promoByID) {
+      res.status(200).json({ data: promoByID });
+    } else {
+      res.status(404).json("Promotion doesn't exist");
     }
-    catch(error){
-        res.status(500).send("Failed to fetch promotions");
-    }   
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Failed to fetch promotion",
+    });
+  }
+};
 
 /**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
  */
-const getPromotionByID = async (req, res)=>{
-    try{
-        const promoID = req.params.id;
-        const promoByID = await Promo.findByPk(promoID);
-        if(promoByID){
-             res.status(200).json({data: promoByID});
-        }else{
-            res.status(404).json("Promotion doesn't exist");
-        }   
-    }
-    catch(error){
-        console.error(error);
-        res.status(500).json({
-            error: "Failed to fetch promotion" 
-        });
-    } 
-}
-
-/**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
- */
-const updatePromotion = async (req, res)=>{
-    try{
-        const promoID = Number.parseInt(req.params.id);
-        const updates = req.body;
-        // check existance of promotion
-        var promotion = await Promo.findByPk(promoID);
-        if(!promotion){
-           return  res.status(404).json({
-                message : "Promotion not found"
-            });
-        }
-        // update
-        promotion = await promotion.update(updates);
-        res.status(200).json({
-                message: "Promotion updated",
-                promotion
-        });
-    }
-    catch(error){
-        res.status(500).send("Could not update, error occured");
-    }
-}
-
-/**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
- */
-const deletePromotion = async (req, res)=>{
+const updatePromotion = async (req, res) => {
+  try {
     const promoID = Number.parseInt(req.params.id);
-    try{
-        let promo = await Promo.findByPk(promoID);
-        // check existance of promotion
-        if(!promo){
-            res.status(404).json({ msg: "The Promotion does not exist" });
-            return;
-        }
-        // delete promotion
-        await promo.destroy();
-        res.sendStatus(204);
+    const updates = req.body;
+    // check existance of promotion
+    var promotion = await Promo.findByPk(promoID);
+    if (!promotion) {
+      return res.status(404).json({
+        message: "Promotion not found",
+      });
     }
-    catch(e){
-        console.error(e);
-        res.status(500).send( error , "Could not delete, error occured");
-    }
-}
+    // update
+    promotion = await promotion.update(updates);
+    res.status(200).json({
+      message: "Promotion updated",
+      promotion,
+    });
+  } catch (error) {
+    res.status(500).send("Could not update, error occured");
+  }
+};
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const deletePromotion = async (req, res) => {
+  const promoID = Number.parseInt(req.params.id);
+  try {
+    let promo = await Promo.findByPk(promoID);
+    // check existance of promotion
+    if (!promo) {
+      res.status(404).json({ msg: "The Promotion does not exist" });
+      return;
+    }
+    // delete promotion
+    await promo.destroy();
+    res.sendStatus(204);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(error, "Could not delete, error occured");
+  }
+};
+
+/**
+ * Get Promotions by sectionID
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const getPromotionsBySectionID = async (req, res) => {
+  const sectionID = Number.parseInt(req.params.sectionID);
+  console.log(sectionID);
+
+  try {
+    const promotions = await Promo.findAll({
+      include: {
+        model: Item,
+        include: {
+          model: Rack,
+          include: {
+            model: Section,
+            where: { id: sectionID },
+          },
+        },
+      },
+      attributes: ["desc"],
+    });
+    console.log(promotions);
+
+    const promotionDescriptions = promotions.map((promo) => promo.desc);
+
+    res.status(200).json({
+      promotionDescriptions,
+    });
+  } catch (error) {
+    console.error("Error fetching promotions: ", error); // Log the error
+    res.status(500).json({
+      error: "Failed to fetch promotions",
+    });
+  }
+};
 
 module.exports = {
-    testPromotion, 
-    createPromotion,
-    getPromotions,
-    getPromotionByID,
-    updatePromotion,
-    deletePromotion
+  testPromotion,
+  createPromotion,
+  getPromotions,
+  getPromotionByID,
+  updatePromotion,
+  deletePromotion,
+  getPromotionsBySectionID,
 };
