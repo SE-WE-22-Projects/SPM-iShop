dfef6de96d5883695e8348509df38f475155bcf9
yehan2002 <yehanjaya2002@gmail.com>
2024-10-03 11:49:46 +0530

    Fix test.

--- client/app/src/test/java/io/github/yehan2002/ishop/ShopMapTest.kt
+++ client/app/src/test/java/io/github/yehan2002/ishop/ShopMapTest.kt
@@ -1,6 +1,7 @@
 package io.github.yehan2002.ishop
 
 import io.github.yehan2002.ishop.navigation.ShopMap
+import io.github.yehan2002.ishop.navigation.aruco.Tag
 import io.github.yehan2002.ishop.util.Point2D
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -12,8 +13,29 @@ class ShopMapTest {
         val markerPos = Point2D(100.0, 100.0)
         map.markers[1] = markerPos
 
-        assertEquals(markerPos, map.estimatePos(1, 0.0, 0.0))
-        assertEquals(markerPos.add(100.0, 0.0), map.estimatePos(1, 100.0, 0.0))
-        assertEquals(markerPos.add(0.0, 100.0), map.estimatePos(1, 100.0, 90.0))
+        assertEquals(
+            markerPos,
+            map.estimatePos(
+                1,
+                0.0,
+                Tag.Rotation(yaw = Math.toRadians(0.0), roll = 0.0, pitch = 0.0)
+            )
+        )
+        assertEquals(
+            markerPos.add(100.0, 0.0),
+            map.estimatePos(
+                1,
+                100.0,
+                Tag.Rotation(yaw = Math.toRadians(0.0), roll = 0.0, pitch = 0.0)
+            )
+        )
+        assertEquals(
+            markerPos.add(0.0, 100.0),
+            map.estimatePos(
+                1,
+                100.0,
+                Tag.Rotation(yaw = Math.toRadians(90.0), roll = 0.0, pitch = 0.0)
+            )
+        )
     }
 }
