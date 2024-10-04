44d05c8e178d95a4705f622e2fef61cf079e09b5
yehan2002 <yehanjaya2002@gmail.com>
2024-10-04 14:41:49 +0530

    Add search query to retrofit.

--- client/app/src/main/java/io/github/yehan2002/ishop/net/ShopService.kt
+++ client/app/src/main/java/io/github/yehan2002/ishop/net/ShopService.kt
@@ -4,6 +4,7 @@ import io.github.yehan2002.ishop.net.dto.MapData
 import io.github.yehan2002.ishop.net.dto.Promotions
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ShopService {
     @GET("/api/promotions/promotion/section/{section}")
@@ -11,5 +12,8 @@ interface ShopService {
 
     @GET("/api/mapping/map")
     suspend fun getMap(): MapData
+
+    @GET("/api/inventory/search")
+    suspend fun searchItem(@Query("q") query: String): Any
 }
 
