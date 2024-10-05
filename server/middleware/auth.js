8dc0431eeeec9c9e1606e29ec5e19b1695ffa5bf
Kasun Hettiarachchi <kasunh1411@gmail.com>
2024-10-05 14:11:53 +0530

    Remove auth on android app endpoints.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,41 +1,48 @@
 const express = require("express");
-const jwt = require('jsonwebtoken');
-const {secretKey} = require('../util/jwtConfig');
+const jwt = require("jsonwebtoken");
+const { secretKey } = require("../util/jwtConfig");
 
 /**
- * 
- * @param {express.Request} req 
- * @param {express.Response} res  
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
  */
 const auth = async (req, res, next) => {
-    let token;
-    if (req.path === "/auth/login") {
-        return next();
-    }
-    
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer: ')
-    ) {
-        try {
-            token = req.headers.authorization.split(' ')[1];
+  let token;
+  console.log(req.path);
+  if (
+    req.path === "/auth/login" ||
+    req.path.startsWith("/mapping") ||
+    req.path.startsWith("/test") ||
+    req.path.startsWith("/promotions/promotion/section") ||
+    req.path.startsWith("/inventory/search")
+  ) {
+    return next();
+  }
 
-            const decoded = jwt.verify(token, secretKey, { algorithms: ["HS512"] });
-            // console.log(token);
-            // req.user = await User.findById(decoded.id).select('-password');
-            res.locals.token = decoded;
-            next();
-        } catch (error) {
-            console.error(error);
-            res.status(401).json({ message: 'Not authorized, token failed' });
-        }
-    }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer: ")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-    if (!token) {
-        res.status(401).json({ message: 'Not authorized, no token' });
+      const decoded = jwt.verify(token, secretKey, { algorithms: ["HS512"] });
+      // console.log(token);
+      // req.user = await User.findById(decoded.id).select('-password');
+      res.locals.token = decoded;
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Not authorized, token failed" });
     }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
 };
 
 module.exports = {
-    auth
-}
+  auth,
+};
